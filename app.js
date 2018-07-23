9edcc31d194c1412798556891c1ef7238faa4472
Camilo Andres Salinas Martinez <camilo@ggitapps.com>
2018-07-23 00:24:21 -0500

    Cambios y pruebas

--- app.js
+++ app.js
@@ -9,10 +9,32 @@ const port = process.env.PORT || 8080;
 app.get('/', function(req, res) {
     var prefix =req.query.prefix;
     var nrc =req.query.nrc;
-    res.send("Hola prefix:"+prefix+"nrc:"+nrc);
+    respuesta = asignarAJson(prefix,nrc);
+    if(respuesta==""){
+        res.send("prefijo incorrecto");
+    }else{
+        res.send("nrc:"+respuesta[0]+"Total:"+respuesta[1]+"Libres:"[2])
+    }
+    
 });
 function asignarAJson(prefix,nrc){
-    
+    try{
+        var obj = JSON.parse(fs.readFileSync('json/'+prefix+'.json', 'utf8'));
+        var nome=[];
+        obj.forEach(element => {
+            nome=Object.values(element);
+        });
+        var retorno= [];
+        nome.forEach(element=>{
+            if(nrc== element[0]){
+                retorno= [element[0],element[1],element[2]];
+            }
+        });
+        return retorno;
+
+    }catch(e){
+        return "";
+    }
 }
 
 
