03e5ea2fbacbf04cc418ce9de06d4f358b4364b2
Camilo Andres Salinas Martinez <camilo@ggitapps.com>
2018-07-23 02:08:43 -0500

    Cambios y pruebas

--- app.js
+++ app.js
@@ -12,27 +12,34 @@ app.get('/', function(req, res) {
     var prefix =req.query.prefix;
     var nrc =req.query.nrc;
     scrapper.scrappearValores(prefix);
-    respuesta = asignarAJson(prefix,nrc);
+    console.log("scrapeando");
+    setTimeout(function(){
+        respuesta = asignarAJson(prefix,nrc);
     if(respuesta==""){
         res.send("prefijo incorrecto");
     }else{
         res.send("nrc:"+respuesta[0]+"Total:"+respuesta[1]+"Libres:"[2])
     }
+    },12*1000)
+    
     
 });
 function asignarAJson(prefix,nrc){
     try{
         var obj = JSON.parse(fs.readFileSync('json/'+prefix+'.json', 'utf8'));
+        console.log("encontro el archivo");
         var nome=[];
         obj.forEach(element => {
             nome=Object.values(element);
         });
+        console.log("todo salio bien");
         var retorno= [];
         nome.forEach(element=>{
             if(nrc== element[0]){
                 retorno= [element[0],element[1],element[2]];
             }
         });
+        console.log("Retorno :3");
         return retorno;
 
     }catch(e){
