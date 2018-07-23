dca4cffa06482b4412cf531657ca74d16d34b573
Camilo Andres Salinas Martinez <camilo@ggitapps.com>
2018-07-23 00:45:45 -0500

    Cambios y pruebas

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const fs = require('fs');
+var scrapper = require('./scrapper.js')
+var fs = require('fs');
 const app = express();
 const port = process.env.PORT || 8080;
 
@@ -36,6 +38,44 @@ function asignarAJson(prefix,nrc){
         return "";
     }
 }
+
+
+
+
+
+
+// var objeto = JSON.parse(fs.readdirSync())
+var obj = JSON.parse(fs.readFileSync('json/cursos.json', 'utf8'));
+var names = [];
+obj.forEach(element => {
+    names.push(Object.values(element)[0])
+
+});
+
+
+
+
+setInterval(function(){scrapGeneral()}, 5*60*1000)
+
+const timeout = milliseconds =>
+    new Promise((resolve, _) => setTimeout(resolve, milliseconds));
+
+
+async function scrapGeneral () {
+    for(var i = 0; i<names.length; i+=4){
+    scrapper.scrappearValores(names[i]);
+    scrapper.scrappearValores(names[i+1]);
+    scrapper.scrappearValores(names[i+2]);
+    scrapper.scrappearValores(names[i+3]);
+    if(i== 40){
+        await timeout(3000);
+        scrapper.scrappearValores(names[i+4]);
+    }    
+    await timeout(10000);
+    }
+
+}
+
 
 
 
