56ed0477557a6599491bfdb9cc49fdcd7ec239aa
Camilo Andres Salinas Martinez <camilo@ggitapps.com>
2018-07-23 22:34:05 -0500

    Cambios y pruebas

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ var scrapper = require('./scrapper.js')
 
 const app = express();
 const port = process.env.PORT || 8080;
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 
 //-----------------------
@@ -31,9 +35,10 @@ app.get('/', function(req, res) {
             respuesta = asignarAJson(prefix,nrc);
             console.log(respuesta);
         if(respuesta==""){
+            console.log("entro al -----")
             res.send("prefijo incorrecto");
         }else{
-            
+            console.log("entro al +++++")
             res.json(respuesta);
             console.log(JSON.stringify(respuesta))
         }
@@ -42,10 +47,11 @@ app.get('/', function(req, res) {
         respuesta = asignarAJson(prefix,nrc);
         console.log(respuesta);
         if(respuesta==""){
+            console.log("entro al ******")
             let error= "prefijo incorrecto";
             res.json(error);
         }else{
-        
+            console.log("entro al //////")
             res.json(respuesta);
             console.log(JSON.stringify(respuesta))
         }   
@@ -67,17 +73,17 @@ function asignarAJson(prefix,nrc){
             nome.push(Object.values(element));
             
         });
-        
+        console.log(nome)
         var retorno= [];
         nome.forEach(element=>{
-            
-
-            if(nrc== element[0]){
-                
-                retorno= [element[0],element[1].capacidad,element[1].disponible];
+            console.log("entró")
+            if(nrc == element[0]){
+                retorno.push(element[0])
+                retorno.push(element[1].capacidad)
+                retorno.push(element[2].disponible)
             }
         });
-        
+        console.log(retorno.length);
         return retorno;
 
     }catch(e){
